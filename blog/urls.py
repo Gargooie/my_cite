addc22788d9c43b134e18ec566b36bd72f545ea3
Dunaeff Eugenii <56464554+Gargooie@users.noreply.github.com>
2022-01-15 14:09:20 +0400

    commit

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,5 @@
-from django.urls import path, re_path
+from django.urls import path
 
-from django.views.static import serve
-from django.conf import settings
 
 from . import views
 
@@ -11,10 +9,8 @@ urlpatterns=[
     path("best", views.AddFavoriteView.as_view(), name="favorites"),
     path("posts", views.postsView.as_view(), name="posts-page"),
      # path("posts/favorite", views.AddFavoriteView.as_view()),
-    path("posts/<slug:slug>",views.PostDetailView.as_view(), name="post_detail_page"),
+    path("posts/<slug:slug>",views.PostDetailView.as_view(), name="post_detail_page")
 
-    re_path(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
-    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 
 
 ]
